fc1fdf45acc2c50c6ace4722598792c4dbcce1ec
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-14 23:32:22 +0100

    EDIT: [Script] Move Menu to namespace

--- Assets/DiegeticMainMenu/_Scripts/Menu.cs
+++ Assets/DiegeticMainMenu/_Scripts/Menu.cs
@@ -1,22 +1,25 @@
 using Unity.Cinemachine;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
+namespace DiegeticMainMenu
 {
-    private const int ActivatedPriority = 10;
-    
-    [SerializeField] private CinemachineCamera cinemachineCamera;
-
-    private int _cachedPriority;
-
-    private void Awake()
+    public class Menu : MonoBehaviour
     {
-        _cachedPriority = cinemachineCamera.Priority;
-    }
+        private const int ActivatedPriority = 10;
 
-    public void SetActive(bool active)
-    {
-        cinemachineCamera.gameObject.SetActive(active);
-        cinemachineCamera.Priority = active ? ActivatedPriority : _cachedPriority;
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+
+        private int _cachedPriority;
+
+        private void Awake()
+        {
+            _cachedPriority = cinemachineCamera.Priority;
+        }
+
+        public void SetActive(bool active)
+        {
+            cinemachineCamera.gameObject.SetActive(active);
+            cinemachineCamera.Priority = active ? ActivatedPriority : _cachedPriority;
+        }
     }
 }
