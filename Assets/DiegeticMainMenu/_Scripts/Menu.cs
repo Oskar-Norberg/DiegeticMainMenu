13f481dac4d2d4bb2d8820124759a327803018a4
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-14 23:20:12 +0100

    EDIT: [Script] Properly set camera priority and active

--- Assets/DiegeticMainMenu/_Scripts/Menu.cs
+++ Assets/DiegeticMainMenu/_Scripts/Menu.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    private const int ActivatedPriority = 10;
+    
     [SerializeField] private CinemachineCamera cinemachineCamera;
+
+    private int _cachedPriority;
+
+    private void Awake()
+    {
+        _cachedPriority = cinemachineCamera.Priority;
+    }
 
     public void SetActive(bool active)
     {
-        cinemachineCamera.gameObject.SetActive(true);
+        cinemachineCamera.gameObject.SetActive(active);
+        cinemachineCamera.Priority = active ? ActivatedPriority : _cachedPriority;
     }
 }
