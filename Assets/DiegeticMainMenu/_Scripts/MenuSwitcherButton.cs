37d649ea1f4f71ec479347761cb7ad393815a7d2
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-14 23:07:20 +0100

    EDIT: [Script] Subscribe to button component in MenuSwitcher

--- Assets/DiegeticMainMenu/_Scripts/MenuSwitcherButton.cs
+++ Assets/DiegeticMainMenu/_Scripts/MenuSwitcherButton.cs
@@ -1,8 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class MenuSwitcherButton : MonoBehaviour
 {
     [SerializeField] private Menu menuToGoTo;
+
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(SwitchMenu);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(SwitchMenu);
+    }
 
     public void SwitchMenu()
     {
