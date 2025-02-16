9162bda9a0b66fc4e03d8e9fa2cd86575ba254dd
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-16 14:02:14 +0100

    ADD: [Script] MenuNavigation

---
+++ Runtime/MenuNavigation.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+namespace DiegeticMainMenu
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class MenuNavigation : MonoBehaviour
+    {
+    }
+}
