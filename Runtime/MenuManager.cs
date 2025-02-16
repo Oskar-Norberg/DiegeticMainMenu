ec6383b25753b82a927f8e2f602483f8d45bc65e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-16 01:53:01 +0100

    EDIT: [Script] Base MenuManager on a stack structure

--- Runtime/MenuManager.cs
+++ Runtime/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DiegeticMainMenu.Singleton;
 using UnityEngine;
 
@@ -7,21 +8,31 @@ namespace DiegeticMainMenu
     {
         [SerializeField] private Menu startingMenu;
 
-        private Menu _currentMenu;
+        private Stack<Menu> _menuStack = new Stack<Menu>();
 
         protected override void Awake()
         {
             base.Awake();
 
-            SwitchMenu(startingMenu);
+            EnterSubMenu(startingMenu);
         }
 
-        public void SwitchMenu(Menu menu)
+        public void EnterSubMenu(Menu menu)
         {
-            _currentMenu?.SetActive(false);
-            menu.SetActive(true);
-
-            _currentMenu = menu;
+            if (_menuStack.Count > 0)
+                _menuStack.Peek()?.SetActive(false);
+            
+            _menuStack.Push(menu);
+            _menuStack.Peek().SetActive(true);
+        }
+        
+        public void Back()
+        {
+            if (_menuStack.Count <= 1)
+                return;
+            
+            _menuStack.Pop().SetActive(false);
+            _menuStack.Peek().SetActive(true);
         }
     }
 }
