fb4e6533dca2b8010cc23f32cd98020ec1161dd5
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-16 15:58:47 +0100

    EDIT: [Script] Add onBlendFinished Event

--- Runtime/MenuManagerCameraEvents.cs
+++ Runtime/MenuManagerCameraEvents.cs
@@ -6,19 +6,27 @@ using UnityEngine.Events;
 public class MenuManagerCameraEvents : MonoBehaviour
 {
     [SerializeField] public UnityEvent onBlendStarted;
+    [SerializeField] public UnityEvent onBlendFinished;
     
     private void OnEnable()
     {
         CinemachineCore.BlendCreatedEvent.AddListener(BlendStarted);
+        CinemachineCore.BlendFinishedEvent.AddListener(BlendFinished);
     }
 
     private void OnDisable()
     {
         CinemachineCore.BlendCreatedEvent.RemoveListener(BlendStarted);
+        CinemachineCore.BlendFinishedEvent.RemoveListener(BlendFinished);
     }
 
     private void BlendStarted(CinemachineCore.BlendEventParams blendEventParams)
     {
         onBlendStarted.Invoke();
+    }
+
+    private void BlendFinished(ICinemachineMixer cinemachineMixer, ICinemachineCamera cinemachineCamera)
+    {
+        onBlendFinished.Invoke();
     }
 }
